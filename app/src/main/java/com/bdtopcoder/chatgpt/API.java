4cb2c19df9a8e1d5f913c3c352276fb36b72615c
ACER <kaushik.sumit7it@gmail.com>
2023-04-14 15:54:39 +0530

    Chat BOT/GPT Application

--- app/src/main/java/com/bdtopcoder/chatgpt/API.java
+++ app/src/main/java/com/bdtopcoder/chatgpt/API.java
@@ -2,5 +2,5 @@ package com.bdtopcoder.chatgpt;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "YOUR_API_KEY";
 }
